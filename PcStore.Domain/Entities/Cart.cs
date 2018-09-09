05b990b77a02da2376b5756409029e5980381408
Ahmed Hassan <a7medmhhh@hotmail.com>
2018-09-09 18:33:59 +0200

    Shopping Cart Cart class

---
+++ PcStore.Domain/Entities/Cart.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PcStore.Domain.Entities
+{
+    class Cart
+    {
+        private List<CardLine> lineCollection = new List<CardLine>();
+        public void Additem(Product product,int quantity = 1)
+        {
+            CardLine line = lineCollection.Where(b => b.products.Id == product.Id)
+                            .FirstOrDefault();
+            if (line == null)
+            {
+                lineCollection.Add(new CardLine { products = product, Quantity = quantity });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+        public void Removeline(Product product)
+        {
+            lineCollection.RemoveAll(b => b.products.Id == product.Id);
+        }
+        public decimal ComputeTotalValue()
+        {
+            return lineCollection.Sum(b => b.products.Price * b.Quantity);
+        }
+        public void Clear()
+        {
+            lineCollection.Clear();
+        }
+        public IEnumerable<CardLine> lines
+        {
+            get
+            {
+                return lineCollection;
+            }
+        }
+    }
+    public class CardLine
+    {
+        public Product products { get; set; }
+        public int Quantity { get; set; }
+    }
+}
